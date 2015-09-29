0e018b1c825d7c64efaccbd071d1cb77ec5da22c
Will Mcginnis <wdm0006@auburn.edu>
2015-09-28 20:34:13 -0400

    adding psycopg2

--- myflaskapp/settings.py
+++ myflaskapp/settings.py
@@ -25,7 +25,6 @@ class ProdConfig(Config):
     """Production configuration."""
     ENV = 'prod'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/example'  # TODO: Change me
     DEBUG_TB_ENABLED = False  # Disable Debug toolbar
 
 
